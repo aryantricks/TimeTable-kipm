fccbd79dce676e2166e3db810e1b431cf2cf58cf
Aryan Mishra <arycodes@gmail.com>
2024-11-17 12:37:07 +0530

    Initial

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'my-app-cache';
+const urlsToCache = [
+    '/',
+    '/index.html'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((cachedResponse) => {
+                return cachedResponse || fetch(event.request);
+            })
+    );
+});
