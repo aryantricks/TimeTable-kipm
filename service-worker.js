e5ae481e6ef1a3d1b93643290b43ef3bd1620a3e
Aryan Mishra <arycodes@gmail.com>
2025-04-22 07:42:10 +0530

    update

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,23 @@
-const CACHE_NAME = 'my-app-cache-v1';
-const urlsToCache = [
-    '/',
-    '/index.html'
-];
+// const CACHE_NAME = 'my-app-cache-v1';
+// const urlsToCache = [
+//     '/',
+//     '/index.html'
+// ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
+// self.addEventListener('install', (event) => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME)
+//             .then((cache) => {
+//                 return cache.addAll(urlsToCache);
+//             })
+//     );
+// });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((cachedResponse) => {
-                return cachedResponse || fetch(event.request);
-            })
-    );
-});
+// self.addEventListener('fetch', (event) => {
+//     event.respondWith(
+//         caches.match(event.request)
+//             .then((cachedResponse) => {
+//                 return cachedResponse || fetch(event.request);
+//             })
+//     );
+// });
